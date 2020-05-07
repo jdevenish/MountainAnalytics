cd0facc77a8dad58c085ffe13f1b3f605227d7b2
Justin Devenish <justin.devenish@gmail.com>
2020-05-07 13:23:59 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -6,7 +6,7 @@ let userObj = {
 window.onload = function () {
     userObj.loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart; 
     userObj.screenWidth = screen.width
-    userObj.geolocation = navigator.geolocation.getCurrentPosition((Geolocation)=> return Geolocation)
+//     userObj.geolocation = navigator.geolocation.getCurrentPosition((Geolocation)=> return Geolocation)
   
     console.log(userObj)
 }
