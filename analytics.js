07c42d291a16de969146d935dd547933f1781bb1
Justin Devenish <justin.devenish@gmail.com>
2020-05-07 13:21:44 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,13 @@
-function logScreenWidth() {
-  console.log("width = ", screen.width)
+let userObj = {
+  loadTime: 0,
+  screenWidth: 0,
+  geolocation: []
+}
+window.onload = function () {
+    userObj.loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart; 
+    userObj.screenWidth = screen.width
+    userObj.geolocation = navigator.geolocation.getCurrentPosition((Geolocation)=> return Geolocation)
+  
+    console.log(userObj)
 }
 
-logScreenWidth()
